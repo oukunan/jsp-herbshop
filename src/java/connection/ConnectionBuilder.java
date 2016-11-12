b3c3fbb3a579bda8c627dc93e5de0a8585279196
zecereal <zcrltv@gmail.com>
2016-11-13 00:58:17 +0700

    add connectionbuilder DB server

--- src/java/connection/ConnectionBuilder.java
+++ src/java/connection/ConnectionBuilder.java
@@ -8,13 +8,15 @@ package connection;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
  * @author KN
  */
 public class ConnectionBuilder {
-    
+
     public static Connection getConnection() {
         Connection conn = null;
         try {
@@ -28,4 +30,18 @@ public class ConnectionBuilder {
         return conn;
     }
 
+    public static Connection getConnectionDBServer() {
+        Connection conn = null;
+        try {    
+            Class.forName("com.mysql.jdbc.Driver");
+            String username = "b97ca0009798e6";
+            String password = "b5695d75";
+            conn = DriverManager.getConnection("jdbc:mysql://ap-cdbr-azure-southeast-b.cloudapp.net/acsm_6a0f73f9a6308a0", username, password);
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(ConnectionBuilder.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SQLException ex) {
+            Logger.getLogger(ConnectionBuilder.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return conn;
+    }
 }
