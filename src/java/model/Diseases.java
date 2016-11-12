3f8234e6bdb7ecf4852866d1321b71f600a6b101
Tatchai Samutsopakul <Tatchai Samutsopakul>
2016-11-12 16:33:01 +0700

    Add Diseases Model

---
+++ src/java/model/Diseases.java
@@ -0,0 +1,112 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+import connection.ConnectionBuilder;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author theca
+ */
+public class Diseases {
+    private int diseId;
+    private String diseName;
+    private String diseType;
+    private String positionInBody;
+    private String diseDetail;
+    private final static String SQL_SEARCH_DISEASES_BY_NAME = "SELECT * FROM DISEASES WHERE LOWER(diseName) LIKE ?"; 
+
+    public Diseases() {
+    }
+
+    public Diseases(int diseId, String diseName, String diseType, String positionInBody, String diseDetail) {
+        this.diseId = diseId;
+        this.diseName = diseName;
+        this.diseType = diseType;
+        this.positionInBody = positionInBody;
+        this.diseDetail = diseDetail;
+    }
+
+    public int getDiseId() {
+        return diseId;
+    }
+
+    public void setDiseId(int diseId) {
+        this.diseId = diseId;
+    }
+
+    public String getDiseName() {
+        return diseName;
+    }
+
+    public void setDiseName(String diseName) {
+        this.diseName = diseName;
+    }
+
+    public String getDiseType() {
+        return diseType;
+    }
+
+    public void setDiseType(String diseType) {
+        this.diseType = diseType;
+    }
+
+    public String getPositionInBody() {
+        return positionInBody;
+    }
+
+    public void setPositionInBody(String positionInBody) {
+        this.positionInBody = positionInBody;
+    }
+
+    public String getDiseDetail() {
+        return diseDetail;
+    }
+
+    public void setDiseDetail(String diseDetail) {
+        this.diseDetail = diseDetail;
+    }
+    
+    public static List<Diseases> searchDiseasesByName(String name){
+        List<Diseases> dises = null;
+        ResultSet rs = null;
+        Diseases dise = null;
+        Connection con = ConnectionBuilder.getConnection();
+        try {
+            PreparedStatement ps = con.prepareStatement(SQL_SEARCH_DISEASES_BY_NAME);
+            ps.setString(1, "%" + name.toLowerCase() + "%");
+            rs = ps.executeQuery();
+            if (rs != null) {
+                if (dises == null) {
+                    dises = new ArrayList<Diseases>();
+                }
+                dise = new Diseases();
+                dise.ORM(rs, dise);
+                dises.add(dise);
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(Herb.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return dises;
+    }
+    
+    public static void ORM(ResultSet rs, Diseases d) throws SQLException {
+        d.setDiseId(rs.getInt("diseId"));
+        d.setDiseName(rs.getString("diseName"));
+        d.setDiseType(rs.getString("diseType"));
+        d.setPositionInBody(rs.getString("positionInBody"));
+        d.setDiseDetail(rs.getString("diseDetail"));
+    }
+    
+}
--- src/java/model/Herb.java
+++ src/java/model/Herb.java
@@ -20,7 +20,7 @@ import java.util.logging.Logger;
  * @author theca
  */
 public class Herb {
-
+    
     private int herbId;
     private String herbName;
     private double herbPrice;
@@ -28,10 +28,10 @@ public class Herb {
     private String herbType;
     private String herbDetail;
     private final static String SQL_SEARCH_HERB_BY_NAME = "SELECT * FROM HERB WHERE LOWER(herbName) LIKE ?";
-
+    
     public Herb() {
     }
-
+    
     public Herb(int herbId, String herbName, double herbPrice, int herbAmount, String herbType, String herbDetail) {
         this.herbId = herbId;
         this.herbName = herbName;
@@ -40,55 +40,55 @@ public class Herb {
         this.herbType = herbType;
         this.herbDetail = herbDetail;
     }
-
+    
     public int getHerbId() {
         return herbId;
     }
-
+    
     public void setHerbId(int herbId) {
         this.herbId = herbId;
     }
-
+    
     public String getHerbName() {
         return herbName;
     }
-
+    
     public void setHerbName(String herbName) {
         this.herbName = herbName;
     }
-
+    
     public double getHerbPrice() {
         return herbPrice;
     }
-
+    
     public void setHerbPrice(double herbPrice) {
         this.herbPrice = herbPrice;
     }
-
+    
     public int getHerbAmount() {
         return herbAmount;
     }
-
+    
     public void setHerbAmount(int herbAmount) {
         this.herbAmount = herbAmount;
     }
-
+    
     public String getHerbType() {
         return herbType;
     }
-
+    
     public void setHerbType(String herbType) {
         this.herbType = herbType;
     }
-
+    
     public String getHerbDetail() {
         return herbDetail;
     }
-
+    
     public void setHerbDetail(String herbDetail) {
         this.herbDetail = herbDetail;
     }
-
+    
     public static List<Herb> searchHerbByName(String name) {
         List<Herb> herbs = null;
         ResultSet rs = null;
@@ -111,11 +111,13 @@ public class Herb {
         }
         return herbs;
     }
-
+    
     public static void ORM(ResultSet rs, Herb h) throws SQLException {
         h.setHerbId(rs.getInt("herbId"));
         h.setHerbName(rs.getString("herbName"));
         h.setHerbPrice(rs.getDouble("herbPrice"));
         h.setHerbAmount(rs.getInt("herbAmount"));
+        h.setHerbType(rs.getString("herbType"));
+        h.setHerbDetail(rs.getString("herbDetail"));
     }
 }
