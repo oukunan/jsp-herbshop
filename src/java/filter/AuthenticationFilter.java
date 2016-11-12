951fe2d180d6fdedaaffb33e11a61ee86827b99a
Tatchai Samutsopakul <Tatchai Samutsopakul>
2016-11-12 18:35:20 +0700

    Add Authentication Filter

---
+++ src/java/filter/AuthenticationFilter.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package filter;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+/**
+ *
+ * @author theca
+ */
+public class AuthenticationFilter implements Filter {
+
+    FilterConfig fg;
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        fg = filterConfig;
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        HttpSession s = ((HttpServletRequest)request).getSession(false);
+        if(s == null || s.getAttribute("user") == null){
+            fg.getServletContext().getRequestDispatcher("/login.jsp").forward(request, response);
+        }else{
+            chain.doFilter(request, response);
+        }
+    }
+
+    @Override
+    public void destroy() {
+    }
+
+}
